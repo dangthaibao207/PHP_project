92ee044a8cfb7f16efb20b10805aa69d0dd71000
dangthaibao207 <162944819+dangthaibao207@users.noreply.github.com>
2024-04-10 17:57:15 +0700

    add file

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php include('layouts/header.php');?>
+
+<section class="container my-5 py-5" id="contact">
+    <div class="container text-center mt-5">
+        <h3>Contact Us</h3>
+        <hr class="mx-auto">
+        <p class="w-50 mx-auto">Phone: <span>09000894234</span> </p>
+        <p class="w-50 mx-auto">Email: <span>a@gmail.com</span> </p>
+        <p class="w-50 mx-auto">Available 24/7</p>
+
+
+    </div>
+</section>
+
+<?php include('layouts/footer.php'); ?>
